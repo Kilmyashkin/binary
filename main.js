01e04f1efbfa23c35aca19d071a564d94256c44a
spock <akilmyashkin@gmail.com>
2016-05-09 14:50:55 +0300

    Add tests to binary function

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
-function prime(array, target){
-  var min = 0;
-  var max = array.length - 1;
-
+/* Returns either the index of the location in the array,
+  or -1 if the array did not contain the targetValue */
+var doSearch = function(array, targetValue) {
+	var min = 0;
+	var max = array.length - 1;
   var guess;
-
+  var count = 0;
   while(min <= max) {
     guess = Math.floor((max + min)/2);
-    if (array[guess] === target) {
+    count += 1;
+    if (array[guess] === targetValue) {
+      println(count);
       return guess;
-    } else if (array[guess] < target) {
+    } else if (array[guess] < targetValue) {
       min = guess + 1;
+       println(guess);
     } else {
       max = guess - 1;
-    }
   }
-  return -1;
 }
 
-var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
-var result = prime(primes, 53);
-console.log(result);
+
+
+	return -1;
+};
+
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+		41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+var result = doSearch(primes, 73);
+println("Found prime at index " + result);
+
+Program.assertEqual(doSearch(primes, 73), 20);
+Program.assertEqual(doSearch(primes, 2), 0);
+Program.assertEqual(doSearch(primes, 99), -1);
